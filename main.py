835c38c217d830620ed4fb86e53fa9935812837a
Orland <andrei8orlov@rambler.ru>
2024-05-16 18:38:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import test_module as tm
+
+tm.func1()
+tm.func2()
---
+++ test_module.py
@@ -0,0 +1,6 @@
+def func1():
+    print('Hello, world! This is func1 from test_module.')
+
+
+def func2():
+    print('Hello, world! This is func2 from test_module.')
